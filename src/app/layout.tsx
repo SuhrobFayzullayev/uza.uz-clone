857342824e500307243e911f62e98e1ee59c122d
Пользователь <suhrob.coder0604@gmail.com>
2023-04-23 06:16:06 +0300

    hadrgvX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,38 +8,38 @@ import "./globals.scss";
 import PhotoCollection from "./(pages)/(home)/components/photoCollection";
 import DownloadApp from "./(pages)/(home)/components/downloadApp";
 import { usePathname } from "next/navigation";
-import { Analytics } from "@vercel/analytics/react";
+
 function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <html lang="en">
-      <body className="bg-[#F7F7F7]">
-        <Container>
-          <Advertising
-            img="https://ads.uza.uz/www/images/a832188f39ef76299985a21c1b09df26.gif"
-            radius={0}
-            height={110}
-          />
-          <Header />
-          <div className="flex">
-            <LeftSide />
-            <main className="w-full bg-[#F7F7F7]">{children}</main>
-          </div>
-          {pathname == "/" ? (
-            <>
-              <PhotoCollection />
-              <DownloadApp />
-            </>
-          ) : (
-            <></>
-          )}
-          <Footer />
-        </Container>
-
-        <Analytics />
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body className="bg-[#F7F7F7]">
+          <Container>
+            <Advertising
+              img="https://ads.uza.uz/www/images/a832188f39ef76299985a21c1b09df26.gif"
+              radius={0}
+              height={110}
+            />
+            <Header />
+            <div className="flex">
+              <LeftSide />
+              <main className="w-full bg-[#F7F7F7]">{children}</main>
+            </div>
+            {pathname == "/" ? (
+              <>
+                <PhotoCollection />
+                <DownloadApp />
+              </>
+            ) : (
+              <></>
+            )}
+            <Footer />
+          </Container>
+        </body>
+      </html>
+    </>
   );
 }
 
