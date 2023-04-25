e8b1f0927ff6b26904a17b495181d24a749b9edf
Пользователь <suhrob.coder0604@gmail.com>
2023-04-25 22:49:41 +0300

    gsdgsdffdgs

--- next.config.js
+++ next.config.js
@@ -28,6 +28,16 @@ const nextConfig = {
       },
     ],
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
